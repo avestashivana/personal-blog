7fdead93d4274b111860e24cf523234f8b70af3d
hamidreza <avesta_shivana@yahoo.com>
2022-12-08 22:23:16 +0330

    add mobile menu and header

---
+++ assets/JS/script.js
@@ -0,0 +1,11 @@
+//navbar variables
+
+const nav = document.querySelector(".mobile-nav"),
+    navMenuBtn = document.querySelector(".nav-menu-btn"),
+    navCloseBtn = document.querySelector(".nav-close-btn");
+
+//navToggle finction
+
+const navToggleFunc = () => nav.classList.toggle("active");
+navMenuBtn.addEventListener("click", navToggleFunc);
+navCloseBtn.addEventListener("click", navToggleFunc);
